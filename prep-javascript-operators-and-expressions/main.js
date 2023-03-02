041aabe28ba3677ea09cd390f5a01746c9ab483a
Joshua Tenny <joshuaLeeTenny@gmail.com>
2023-03-02 00:27:39 +0000

    completed javascript-operators-and-expressions exercise

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,59 @@
+/*       multiply                          */
+
+var number1 = 5;
+var number2 = 10;
+
+var product = number1 * number2;
+
+console.log(product);
+console.log(typeof product);
+
+/*           subtraction                         */
+
+var charge = 6;
+var payment = 3;
+
+var amountRemaining = charge - payment;
+console.log(amountRemaining);
+console.log(typeof amountRemaining);
+
+/*        calculate average                           */
+
+var test = 100;
+var assignments = 90;
+var final = 84;
+
+var grade = test + assignments + final;
+console.log(grade / 3);
+console.log(typeof grade);
+
+/*          string concatenation                         */
+
+var firstName = 'Joshua';
+var lastName = 'Tenny';
+
+var fullName = firstName + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+
+/*               less than comparison                  */
+
+var pH = 8;
+var isAcidic = pH < 7;
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+/*         equality comparison                          */
+
+var numberOfSoldiers = 150;
+var isSparta = numberOfSoldiers === 300;
+console.log(isSparta);
+console.log(typeof isSparta);
+
+/*         additional assignment                          */
+
+var name = 'Josh';
+var nickname = name + ' is the GOAT';
+
+console.log(nickname);
+console.log(typeof nickname);
---
+++ prep-javascript-operators-and-expressions/prep-javascript-operators-and-expressions-quiz-notes.md
@@ -0,0 +1,38 @@
+# prep-javascript-operators-and-expressions-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What data type is returned by an arithmetic operation?
+- What is string concatenation?
+- What purpose(s) does the `+` plus operator serve in JavaScript?
+- What data type is returned by comparing two values (`<`, `>`, `===`, etc)?
+- What does the `+=` "plus-equals" operator do?
+
+
+## Notes
+
+All student notes should be written here.
+
+
+How to write `Code Examples` in markdown
+
+for JS:
+```javascript
+const data = "Howdy"
+```
+
+for HTML:
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+```css
+div {
+  width:100%
+}
+```
