11aeb98a21129c1bfa3016e700403a670589fb13
Joshua Tenny <joshuaLeeTenny@gmail.com>
2023-03-02 17:12:45 +0000

    completed javascript-operators-and-expressions exercise

--- prep-javascript-operators-and-expressions/main.js
+++ prep-javascript-operators-and-expressions/main.js
@@ -1,59 +1,59 @@
 /*       multiply                          */
 
-var number1 = 5;
-var number2 = 10;
+const number1 = 5;
+const number2 = 10;
 
-var product = number1 * number2;
+const product = number1 * number2;
 
 console.log(product);
 console.log(typeof product);
 
 /*           subtraction                         */
 
-var charge = 6;
-var payment = 3;
+const charge = 6;
+const payment = 3;
 
-var amountRemaining = charge - payment;
+const amountRemaining = charge - payment;
 console.log(amountRemaining);
 console.log(typeof amountRemaining);
 
 /*        calculate average                           */
 
-var test = 100;
-var assignments = 90;
-var final = 84;
+const test = 100;
+const assignments = 90;
+const final = 84;
 
-var grade = test + assignments + final;
+const grade = test + assignments + final;
 console.log(grade / 3);
 console.log(typeof grade);
 
 /*          string concatenation                         */
 
-var firstName = 'Joshua';
-var lastName = 'Tenny';
+const firstName = 'Joshua';
+const lastName = 'Tenny';
 
-var fullName = firstName + lastName;
+const fullName = firstName + lastName;
 console.log(fullName);
 console.log(typeof fullName);
 
 /*               less than comparison                  */
 
-var pH = 8;
-var isAcidic = pH < 7;
+const pH = 8;
+const isAcidic = pH < 7;
 console.log(isAcidic);
 console.log(typeof isAcidic);
 
 /*         equality comparison                          */
 
-var numberOfSoldiers = 150;
-var isSparta = numberOfSoldiers === 300;
+const numberOfSoldiers = 150;
+const isSparta = numberOfSoldiers === 300;
 console.log(isSparta);
 console.log(typeof isSparta);
 
 /*         additional assignment                          */
 
-var name = 'Josh';
-var nickname = name + ' is the GOAT';
+const name = 'Josh';
+const nickname = name + ' is the GOAT';
 
 console.log(nickname);
 console.log(typeof nickname);
